037af3632892d71125ad281d7c04196e437a0d63
Lucas Maupin <lmaupin@voltainc.com>
2020-03-09 16:25:22 -0400

    initial commit of script and repo.

---
+++ README.md
@@ -0,0 +1,1 @@
+This script is meant to be run within Powershell 7. Please run in Powershell 7.0+ to take advantage of the -Parallel function.
---
+++ SysmonReporting.ps1
@@ -0,0 +1,22 @@
+﻿##TO BE RUN IN POWERSHELL 7.0##
+##USES THE PARALLEL SWITCH TO ITERATE THROUGH THE AD LIST IN PARALLEL RUNSPACES##
+
+##Set Execution-Policy scope to the current Powershell process
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$ad = (Get-ADComputer -Filter *).Name
+$ad | ForEach-Object -Parallel{
+$x = $_
+    if(Invoke-Command -ComputerName $x -ScriptBlock {(Get-Service -Name sysmon -ErrorAction SilentlyContinue -Verbose).Status -eq "running"})
+    {
+        $running = "$x is currently RUNNING" | Out-File -FilePath $env:HOMEDRIVE\$env:HOMEPATH\Desktop\sysmonsuccess.log -Encoding UTF-8 -Append -Force
+    }
+
+    elseif(Invoke-Command -ComputerName $x -ScriptBlock {(Get-Service -Name sysmon -ErrorAction SilentlyContinue -Verbose).Status -eq "stopped"})
+    {
+        $stopped = "$x is currently STOPPED" | Out-File -FilePath $env:HOMEDRIVE\$env:HOMEPATH\Desktop\sysmonfailure.log -Encoding UTF-8 -Append -Force
+    }
+    else
+    {
+        $failed = "$x is not currently running sysmon" | Out-File -FilePath $env:HOMEDRIVE\$env:HOMEPATH\Desktop\sysmonfailure.log -Encoding UTF-8 -Append -Force
+    }
+} -ThrottleLimit 20
